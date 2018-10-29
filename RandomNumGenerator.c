31102d308958dad5e92c4bbcac5096dd3ec7141c
Christopher Young <Young.christopher.425@gmail.com>
2018-10-29 11:31:24 -0700

    with integers

--- RandomNumGenerator.c
+++ RandomNumGenerator.c
@@ -17,95 +17,67 @@ int main(int argc, char *argv[]){
 	int B = atoi(argv[2]);
 	int P = atoi(argv[3]);
 	int rank,p;
-	int n = 128;
+	int n = 10;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 	assert(p>=2);
 	assert((n/p)%2 == 0);
-	//Creating a 3d matirx to hold the many M(2x2) matrixies
-	//n x 2 x 2
-	int ***Mmatrix = (int ***)malloc(sizeof(int**)*n/p);
-	//use a temp to store for next timestrp
-	int ***temp = (int ***)malloc(sizeof(int**)*n/p);
-	int i;
-	for (i=0; i < (n/p); i++){
-		Mmatrix[i] = (int **)malloc(sizeof(int *)*2);
-		temp[i] = (int **)malloc(sizeof(int *)*2);
-	}
-	for (i=0; i < (n/p); i++){
-		Mmatrix[i][0] = (int*)malloc(sizeof(int)*2);
-		Mmatrix[i][1] = (int*)malloc(sizeof(int)*2);
-		temp[i][0] = (int*)malloc(sizeof(int)*2);
-		temp[i][1] = (int*)malloc(sizeof(int)*2);
-	}
-	
 
-	Mmatrix[0][0][0] = 1; 
-	Mmatrix[0][0][1] = 0;
-	Mmatrix[0][1][0] = 0;
-	Mmatrix[0][1][1] = 1;
-	for (i=1; i < (n/p); i++){
-		Mmatrix[i][0][0] = A;
-		Mmatrix[i][0][1] = 0;
-		Mmatrix[i][1][0] = B;
-		Mmatrix[i][1][1] = 1;
-	}
-
+	int A[10] = {1,2,3,4,5,6,7,8,9,10};
+	int B[10];
 	if (rank == 4){
-		for(i = 0; i< n/p; i++){
-		printf("%d \n", i);
-		printf("| %d | %d |\n", Mmatrix[i][0][0], Mmatrix[i][0][1]);
-		printf("| %d | %d |\n", Mmatrix[i][1][0], Mmatrix[i][1][1]);
+		for(i = 0; i< n; i++){
+			printf("%d \n", i);
+			printf("| %d |\n", A[i]);
 		}
 	}
-	for(i=0; i < (log((n/p))/log(2)); i++){
+	for(i=0; i < (log(n)/log(2)); i++){
 		int j;
 		#pragma omp parallel for
-		for(j = 0; j < n/p;j++){
+		for(j = 0; j < n;j++){
 			int pw = pow(2,i);
 			if (j<pw){
-				temp[j] = Mmatrix[j];
+				B[j] = A[j];
 			}else{
-				temp[j] = twoby2times2by2(Mmatrix[j],Mmatrix[j-pw]);
+				B[j] = A[j] + A[j-pw];
 			}
 		}
-		memcpy(Mmatrix,temp,sizeof(int**)*(n/p));	
+		memcpy(A,B,sizeof(A));	
 	}
 
 
 	if (rank == 4){
-		for(i = 0; i< n/p; i++){
-		printf("%d \n", i);
-		printf("| %d | %d |\n", Mmatrix[i][0][0], Mmatrix[i][0][1]);
-		printf("| %d | %d |\n", Mmatrix[i][1][0], Mmatrix[i][1][1]);
+		for(i = 0; i< n; i++){
+			printf("%d \n", i);
+			printf("| %d |\n", A[i]);
 		}
 	}
 	
 
-	int** local = Mmatrix[n/p-1];
-	int** global = local;	
+	int local = A[n-1];
+	int global = local;	
 	MPI_Status status;
 	MPI_Request request;
 	for(i=0; i<log(p)/log(2); i++){
 		local = global;
-		int **recvglobal;
+		int recvglobal;
 		MPI_Barrier(MPI_COMM_WORLD);
 		if(rank + pow(2,i) < p){
-			MPI_Send((&global[0][0]),4,MPI_INT,rank + pow(2,i), 0, MPI_COMM_WORLD);
+			MPI_Send(&global,1,MPI_INT,rank + pow(2,i), 0, MPI_COMM_WORLD);
 		}	
 		int j;
 		for(j=0; j<p; j++){
 			if(j+pow(2,i) == rank){
-				MPI_Recv(&(recvglobal[0][0]), 4, MPI_INT, rank-pow(2,i),MPI_ANY_TAG,MPI_COMM_WORLD, &status);
+				MPI_Recv(&recvglobal, 1, MPI_INT, rank-pow(2,i),MPI_ANY_TAG,MPI_COMM_WORLD, &status);
 				if (rank-pow(2,i) < rank){
-					global = twoby2times2by2(global,recvglobal);
+					global = global+recvglobal;
 				}
 			}
 		}
 
 	}
-/*
+
 	int recievedglobal;
 	if(rank+1 < p)
 		MPI_Send(&global, 1 , MPI_INT, rank+1, 0, MPI_COMM_WORLD);
@@ -117,6 +89,7 @@ int main(int argc, char *argv[]){
 	for(i=0;i<n/p;i++){
 		A[i] = A[i] + global;
 	}
+/*
 	for(i=0;i<p;i++){
 		MPI_Barrier(MPI_COMM_WORLD);
 		if(i==rank){
@@ -125,7 +98,7 @@ int main(int argc, char *argv[]){
 			}
 		}
 	}
-
+*/
 
 //
 	if(rank == 1){
@@ -143,7 +116,7 @@ int main(int argc, char *argv[]){
 		printf("The global value we got was %d",local);
 	}
 
-*/
+
 
 
 
